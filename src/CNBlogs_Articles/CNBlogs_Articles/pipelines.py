4842d4612105b5855b372c402e2a42e9582347d4
MaJesTySA <363644935@qq.com>
2019-10-30 15:30:55 +0800

    Update pipelines.py

--- src/CNBlogs_Articles/CNBlogs_Articles/pipelines.py
+++ src/CNBlogs_Articles/CNBlogs_Articles/pipelines.py
@@ -4,8 +4,12 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import codecs
+import json
 
 import pymongo
+from scrapy.exporters import JsonItemExporter
+
 
 class MongoPipeline(object):
     def __init__(self, mongo_url, mongo_db):
@@ -25,8 +29,36 @@ class MongoPipeline(object):
 
     def process_item(self, item, spider):
         name = item.__class__.__name__
-        self.db[name].update({'url':item['url']},{'$set':item},True)
+        self.db[name].update({'url': item['url']}, {'$set': item}, True)
         return item
 
     def close_spider(self, spider):
         self.client.close()
+
+
+class JSONPipeline(object):
+    def __init__(self):
+        self.file = codecs.open('cnBlogsArticles.json', 'w', encoding='utf-8')
+
+    def process_item(self, item, spider):
+        lines = json.dumps(dict(item), ensure_ascii=False) + '\n'
+        self.file.write(lines)
+        return item
+
+    def spider_closed(self, spider):
+        self.file.close()
+
+
+class JSONExportPipeline(object):
+    def __init__(self):
+        self.file = open('ExportedArticles.json', 'wb')
+        self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
